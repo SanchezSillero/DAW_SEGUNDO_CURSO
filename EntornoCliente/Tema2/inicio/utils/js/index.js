dcfa14407b93c029828673e5a5fcac4cb2335d0a
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2025-03-06 18:09:24 +0100

    Update

---
+++ EntornoCliente/Tema2/inicio/utils/js/index.js
@@ -0,0 +1,20 @@
+console.log("Esto es mi codigo JS");
+// comentarios
+/* lineas multiples */
+//shift alt A (comenta y descomenta la linea)
+//shift alt arriba/abajo duplica linea
+/* class
+        variables
+        constructores
+        funciones */
+// no indico tipos en la variable -> JS es un lenguaje NO TIPADO
+
+//variable constante (final en JAVA)-> const
+const NOMBRE_ASIGNATURA = "Desarrollo Cliente";
+NOMBRE_ASIGNATURA = "alksfjhb"; // da error, hay que verlo en la consola del navegador
+
+//variables ambito bloque -> let
+let nombre = "Paco";
+nombre = "Francisco";
+
+//variable global -> var (usar siempre let, es peligroso porque desde cualquier parte del navegador se podra acceder a dicha variable)
