a7833b525e49bb18ea7d0ec49d20409ca4b7c465
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2025-03-07 12:06:53 +0100

    update

---
+++ EntornoCliente/Tema2/funciones/utils/js/index.js
@@ -0,0 +1,9 @@
+let nombre = "Francisco";
+let edad = "33";
+console.log(nombre);
+console.log(`Hola ${nombre}, encantado de saludarte`);
+
+//interaccion del usuario
+// alert(`Bienvenido ${nombre} a la pagina`); -->saca info sin posibilidad de hacer nada
+let nombre1 = prompt("Por favor introduce tu nombre"); // el prompt pide informacion
+alert(`Tu nombre es ${nombre1}`);
